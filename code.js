6b79798f4ba651c9bdc6c27c2d8a8e28dc5fe4c2
Phạm Tiến Đạt <zomodat123@gmail.com>
2024-03-04 14:50:20 +0700

    fix bug

--- code.js
+++ code.js
@@ -91079,7 +91079,7 @@
                 if (myRange) {
                     let RangeBetweenMeAndEnemy = dist2dSQRT(myPlayer, Enemy);
                     if (RangeBetweenMeAndEnemy <= myRange) {
-                      Settings.Aimbot.a = calcAngle(myPlayer, EnemyToAim, true);
+                      Settings.Aimbot.a = calcAngle(myPlayer, Enemy, true);
                       let e = 2 * Math.PI;
                       let Angle255 = Math.floor(
                         (((Settings.Aimbot.a + e) % e) * 255) / e,
